c6e07d28d9d1f9da506066e782d678ee6f2df36c
Yashvardhan <yashvardhansingh1009@gmail.com>
2026-07-16 20:12:44 +0530

    Added new files in javascript

--- 03._Javascript/hello.js
+++ 03._Javascript/hello.js
@@ -2,4 +2,17 @@ function printChai() {
     console.log("Hello chai")
 }
 
-function 
+function bringBrush(numberOfBrush) {
+    // `` is called backticks and is used when you want to add your paramter as it is between the comments somewhere
+    // use ${parameter name}
+    console.log(`Hanji le aaya ${numberOfBrush} brush`)
+}
+
+function addTwoNum(num1, num2) {
+    return num1 + num2
+}
+
+printChai();
+bringBrush(4);
+
+console.log(addTwoNum(2, 3));
